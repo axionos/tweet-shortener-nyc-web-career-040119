3f72a8a46d93a55ed87bee4c42fb9189514f09ee
SOYEONG OH <axionoso@gmail.com>
2019-03-27 15:55:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,35 +14,17 @@ def dictionary
 end
 
 def word_substituter(string)
-  tweet = string.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
-    end
-  end
-  tweet.join(" ")
+  
 end
 
 def bulk_tweet_shortener (array)
-  array.each do |tweet|
-    puts word_substituter(tweet)
-  end
+
 end
 
 def selective_tweet_shortener (string)
-  if string.length > 140
-    word_substituter(string)
-  else
-    string
-  end
+
 end
 
 def shortened_tweet_truncator (string)
-  shortened = word_substituter(string)
-  if shortened.length > 140
-    shortened[0..134] + "(...)"
-  else
-    string
-  end
+
 end
