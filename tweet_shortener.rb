3ff3eb6b52fd05aa364fca4066a92f7c399763b6
SOYEONG OH <axionoso@gmail.com>
2019-03-27 15:56:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ def dictionary
 end
 
 def word_substituter(string)
-  tweetstring.split.collect do |word|
+  tweet = string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
